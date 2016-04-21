24953ea1b2fc53660415e9037f89e7b3c8e78986
没有bug的无聊世界 <12824031@qq.com>
2016-04-21 23:00:21 +0800

    base commit

---
+++ companies/sketch111.js
@@ -0,0 +1,479 @@
+var particleSystem = [];
+var investorSystem = [];
+var attractors = [];
+var table;
+var aggregated = {};
+var connections = [];
+var investors = [];
+var table2;
+var companiesToDisplay = [];
+var investorsToDisplay = [];
+var investorsParticles = [];
+var investorsAggregated = [];
+var button;
+
+function preload() {
+    table = loadTable("data/investments.csv", "csv", "header");
+    table2 = loadTable("data/companies_categories.csv", "csv", "header");
+}
+
+
+function setup() {
+    var canvas = createCanvas(windowWidth, windowHeight);
+    frameRate(30);
+
+    button = createButton('back');
+    button.position(width / 2 - 60, height / 2 + 300);
+    button.mousePressed(goBack);
+
+    colorMode(HSB, 360, 100, 100, 100);
+    //background(0);
+    //textSize(10);
+    textAlign(CENTER);
+    //textStyle(BOLD);
+
+    //print(table.getRowCount() + " total rows in table");
+
+    /* replace \, by ""
+    aggregates usd amounts per company invested 
+    by using the object aggregated  */
+    for (var r = 0; r < table.getRowCount(); r++) {
+        var cname = table.getString(r, "company_name");
+        var invested = table.getString(r, "amount_usd");
+        var investorname = table.getString(r, "investor_name")
+        invested = parseInt(invested);
+        if (!isNaN(invested)) {
+            if (aggregated.hasOwnProperty(cname)) {
+                aggregated[cname] = aggregated[cname] + invested;
+            } else {
+                aggregated[cname] = invested;
+            }
+        }
+        investorsAggregated[investorname] = "";
+    }
+
+
+
+    /* converts the object into an array of companies */
+    var aAggregated = [];
+    Object.keys(aggregated).forEach(function (name_) {
+        var company = {};
+        company.name = name_;
+        company.sum = aggregated[name_]
+        aAggregated.push(company);
+    });
+
+
+    /* sorts the array by usd amount */
+    aAggregated.sort(function (companyA, companyB) {
+        return companyB.sum - companyA.sum;
+    });
+
+    aAggregated = aAggregated.slice(0, 100);
+
+    var investors = [];
+    Object.keys(investorsAggregated).forEach(function (name) {
+        var investor = {};
+        investor.name = name;
+        investors.push(investor);
+    });
+    
+    //create an investorParticle per each investor, put the investor inside the particle and the particle inside the investors
+    investors.forEach(function(iv){
+        var particle = new investorParticle(iv.name, 12, iv);
+        iv.particle = particle;
+        investorsParticles.push(particle);
+    });
+
+    for (var r = 0; r < table.getRowCount(); r++) {
+        var cname = table.getString(r, "company_name");
+        var invested = table.getString(r, "amount_usd");
+        var investorname = table.getString(r, "investor_name")
+        invested = parseInt(invested);
+
+        var foundCompany = aAggregated.find(function (element) {
+            return element.name == cname;
+        });
+
+        if (foundCompany) {
+
+            var foundInvestor = investors.find(function (element) {
+                return element.name == investorname;
+            });
+
+            if (foundInvestor) {
+
+                var connection = {};
+                connection.company = foundCompany;
+                connection.investor = foundInvestor;
+                connection.amount = invested;
+                connections.push(connection);
+
+            }
+
+        }
+
+    }
+    //console.log(connections);
+    /* prints the top company */
+    //print(aAggregated[0].name + " : " +aAggregated[0].sum);
+
+
+    /* creates 100 particles from the array */
+    for (var i = 0; i < aAggregated.length; i++) {
+        var p = new Particle(aAggregated[i].name, aAggregated[i].sum);
+        particleSystem.push(p);
+        companiesToDisplay.push(p)
+            //print(p);
+    }
+
+    /*for(var i =0; i<connections.length; i++){
+        var p = new Investor(connections[i].investor.name, 5);
+        investorsToDisplay.push(p);
+    }*/
+
+
+
+    /* COUNT THE NUMBER OF CATEGORIES */
+    var ob = {}; //this is the counts
+    for (var i = 0; i < table2.getRowCount(); i++) {
+        var catName = table2.getString(i, "category_code");
+        if (ob.hasOwnProperty(catName)) {
+            ob[catName]++;
+        } else {
+            ob[catName] = 1;
+        }
+    };
+
+  
+
+
+    /* creates a central atractor of strength 1 */
+    var at = new Attractor(createVector(width / 2, height / 2), 1);
+    attractors.push(at);
+
+
+    //print(companiesToDisplay);
+
+
+
+}
+
+
+function draw() {
+    background(0);
+
+    /*checks for pairs of particles*/
+    for (var STEPS = 0; STEPS < 4; STEPS++) {
+        for (var i = 0; i < particleSystem.length - 1; i++) {
+            for (var j = i + 1; j < particleSystem.length; j++) {
+                var pa = particleSystem[i];
+                var pb = particleSystem[j];
+                var ab = p5.Vector.sub(pb.pos, pa.pos);
+                var distSq = ab.magSq();
+                if (distSq <= sq(pa.radius + pb.radius)) {
+                    var dist = sqrt(distSq);
+                    var overlap = (pa.radius + pb.radius) - dist;
+                    ab.div(dist); //ab.normalize();
+                    ab.mult(overlap * 0.5);
+                    pb.pos.add(ab);
+                    ab.mult(-1);
+                    pa.pos.add(ab);
+
+                    pa.vel.mult(0.97);
+                    pb.vel.mult(0.97);
+
+                }
+            }
+        }
+    }
+
+
+
+    for (var i = companiesToDisplay.length - 1; i >= 0; i--) {
+        var p = companiesToDisplay[i];
+        p.update();
+        p.draw();
+    }
+    
+    investorsToDisplay.forEach(function(investorParticle){
+        investorParticle.update();
+        investorParticle.draw();
+    });
+
+    /*   investorsToDisplay.forEach(function(i){
+           i.update();
+           i.draw();
+       })*/
+}
+
+/*COMPANY PARTICLES HERE*/
+var Particle = function (name, sum, category) {
+    this.name = name;
+    this.sum = sum;
+    this.category = category
+
+    this.radius = sqrt(sum) / 4000;
+    var initialRadius = this.radius;
+
+    var isMouseOver = false;
+    var maximumRadius = 70;
+
+    var tempAng = random(TWO_PI);
+    this.pos = createVector(cos(tempAng), sin(tempAng));
+    this.pos.div(this.radius);
+    this.pos.mult(1000);
+    this.pos.set(this.pos.x + width / 2, this.pos.y + height / 2);
+    this.vel = createVector(0, 0);
+    var acc = createVector(0, 0);
+
+    var rowCat = table2.findRow(this.name, "name");
+
+    if (rowCat != null) {
+        this.categoryName = rowCat.get("category_code");
+    } else {
+        //print(this.name);
+    }
+    //print (rowCat);
+
+    switch (this.categoryName) {
+    case "software":
+        this.color = {
+            h: 325,
+            s: 44,
+            b: 99
+        };
+        break;
+    case "web":
+        this.color = {
+            h: 286,
+            s: 37,
+            b: 100
+        };
+        break;
+    case "biotech":
+        this.color = {
+            h: 196,
+            s: 37,
+            b: 100
+        };
+        break;
+    case "mobile":
+        this.color = {
+            h: 76,
+            s: 45,
+            b: 100
+        };
+        break;
+    case "enterprise":
+        this.color = {
+            h: 16,
+            s: 37,
+            b: 100
+        };
+        break;
+    case "ecommerce":
+        this.color = {
+            h: 215,
+            s: 44,
+            b: 99
+        };
+        break;
+    default:
+        this.color = {
+            h: 150,
+            s: 10,
+            b: 100
+        };
+    }
+
+    this.update = function () {
+        checkMouse(this);
+
+        attractors.forEach(function (A) {
+            var att = p5.Vector.sub(A.pos, this.pos);
+            var distanceSq = att.magSq();
+            if (distanceSq > 1) {
+                att.normalize();
+                att.div(10);
+                //att.mult(this.radius*this.radius/200);
+                acc.add(att);
+            }
+        }, this);
+        this.vel.add(acc);
+        this.pos.add(this.vel);
+        acc.mult(0);
+    }
+
+    this.draw = function () {
+        noStroke();
+        if (isMouseOver) {
+            fill(0, 100, 50);
+        } else {
+            fill(0, 0, 50);
+        }
+
+        fill(this.color.h, this.color.s, this.color.b);
+        ellipse(this.pos.x,
+            this.pos.y,
+            this.radius * 2,
+            this.radius * 2);
+
+        if (this.radius == maximumRadius) {
+
+            fill(this.color.h, this.color.s + 40, this.color.b * 0.8);
+            textSize(8);
+            text(this.categoryName, this.pos.x, this.pos.y + 35);
+
+            fill(0, 0, 20);
+            textSize(12);
+            textStyle(BOLD);
+            text(this.name, this.pos.x, this.pos.y - 5);
+            textSize(10);
+            text(this.sum, this.pos.x, this.pos.y + 10);
+
+        }
+    }
+
+    function checkMouse(instance) {
+        var mousePos = createVector(mouseX, mouseY)
+        if (mousePos.dist(instance.pos) <= instance.radius) {
+            incRadius(instance);
+            isMouseOver = true;
+        } else {
+            decRadius(instance);
+            isMouseOver = false;
+        }
+
+    }
+
+    function incRadius(instance) {
+        instance.radius += 4;
+        if (instance.radius > maximumRadius) {
+            instance.radius = maximumRadius;
+        }
+
+    }
+
+    function decRadius(instance) {
+        instance.radius -= 4;
+        if (instance.radius < initialRadius) {
+            instance.radius = initialRadius;
+        }
+    }
+
+    this.getMouseOver = function () {
+        return isMouseOver;
+    }
+}
+
+/*INVESTOR PARTICLES HERE*/
+var investorParticle = function (name, sum, investor) {
+    this.invename = name;
+    this.totalinvested = sum;
+    this.radius = 50;
+    this.investor = investor;
+
+    this.pos = createVector(random(0, width), random(0, height));
+   
+    this.vel = createVector(0, 0);
+    var acc = createVector(0, 0);
+    var isMouseOver = false;
+
+    this.update = function () {
+        //checkMouse(this);
+        //this.opacity = iOpacity;
+
+       /* attractors.forEach(function (A) {
+            var att = p5.Vector.sub(A.getPos(), this.pos);
+            var distanceSq = att.magSq();
+            if (distanceSq > 1) {
+                att.normalize();
+                att.mult(5);
+                att.mult(initialRadius * .001);
+                acc.add(att);
+            }
+
+        }, this);*/
+
+        this.draw = function () {
+            fill(0, 0, 255);
+            noStroke();
+           
+            fill(0, 0, 50);
+            ellipse(this.pos.x, this.pos.y, 40, 40);
+             fill(0, 0, 100);
+            text(this.invename, this.pos.x, this.pos.y);
+        }
+
+    }
+}
+
+
+
+
+function mouseClicked() {
+    var clickedCompany = null;
+
+    companiesToDisplay.forEach(function (co) {
+
+        if (co.getMouseOver()) clickedCompany = co;
+    });
+
+
+    if (clickedCompany != null) {
+        companiesToDisplay = [];
+        companiesToDisplay.push(clickedCompany)
+    }
+    /*else {
+           companiesToDisplay = [];
+           particleSystem.forEach(function (p) {
+               companiesToDisplay.push(p);
+           })
+       };*/
+
+    investorsToDisplay = [];
+    connections.forEach(function (c) {
+        if (clickedCompany != null) {
+            if (c.company.name == clickedCompany.name) {
+                if(!investorsToDisplay.includes(c.investor)){
+                    investorsToDisplay.push(c.investor.particle);
+                }
+                
+            }
+        }
+
+    })
+    //console.log(investorsToDisplay);
+
+};
+
+function goBack() {
+    companiesToDisplay = [];
+    particleSystem.forEach(function (p) {
+        companiesToDisplay.push(p);
+    });
+}
+
+
+
+
+
+var Attractor = function (pos_, s) {
+    this.pos = pos_.copy();
+    var strength = s;
+    this.draw = function () {
+        noStroke();
+        fill(0, 100, 100);
+        ellipse(this.pos.x, this.pos.y,
+            strength, strength);
+    }
+}
+
+
+
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+
+}
---
+++ wars/sketch.js
@@ -0,0 +1,514 @@
+var countries;
+var table;
+var wars = [];
+var lastSelected;
+var selected;
+var hovered;
+var bubbles = {};
+var timelineDots = [];
+var timeLine;
+var slide = false;
+var myCountries = [];
+
+function preload() {
+    table = loadTable("data/Inter-StateWarData_v4.0.csv", "csv", "header");
+    countries = loadJSON("countryShape2.json");
+}
+
+function setup() {
+    var cnv = createCanvas(windowWidth, windowHeight);
+    cnv.mousePressed(timeLineSlideStart);
+    cnv.mouseReleased(timeLineSlideEnd);
+    table.getRows().forEach(function (row) {
+        getWars(row);
+    });
+    getDots();
+    createBubbles();
+    timeLine = new TimeLine(this);
+    countries.features.forEach(function (feature) {
+        var myCountry = {};
+        myCountry.name = feature.properties.NAME;
+        if (feature.geometry != null) {
+
+            if (feature.geometry.type == "Polygon") {
+                myCountry.coordinates = feature.geometry.coordinates;
+            } else if (feature.geometry.type == "MultiPolygon") {
+                var myCountry = {};
+                myCountry.coordinates = [];
+                feature.geometry.coordinates.forEach(function (path) {
+                    myCountry.coordinates.push(path[0]);
+                });
+            }
+            myCountries.push(myCountry);
+        }
+    });
+}
+
+function draw() {
+    background(255);
+    fill('#a5a5a5')
+    stroke(255);
+    strokeWeight(0.5);
+    countries.features.forEach(function (feature) {
+        if (feature.geometry != null) {
+            if (feature.geometry.type == "Polygon") {
+                feature.geometry.coordinates.forEach(function (path) {
+                    beginShape();
+                    path.forEach(function (vertice) {
+                        mercator(vertice[1], vertice[0]);
+                    });
+                    endShape();
+                });
+
+            } else if (feature.geometry.type == "MultiPolygon") {
+                feature.geometry.coordinates.forEach(function (path) {
+                    beginShape();
+                    path[0].forEach(function (vertice) {
+                        mercator(vertice[1], vertice[0]);
+                    });
+                    endShape();
+                });
+            }
+        }
+    });
+    timeLine.draw();
+    timeLine.updateSlideDot(timeLineSlide())
+    //drawLegend();
+    drawBubbles(timeLine.slideDot.decade, this);
+    hovered = false;
+}
+
+
+//function drawLegend() {
+//    var arr = [
+//      {
+//            text: 'Winner',
+//            color: '#F11B82'
+//        },
+//        {
+//            text: 'Loser',
+//            color: '#1a87a7'
+//        },
+//        {
+//            text: 'Compromise/Tied',
+//            color: '#6bc375'
+//        },
+//        {
+//            text: 'The war was transformed' + '\n' + 'into another type of war',
+//            color: '#ffdf00'
+//        }
+//    ];
+//
+//    arr.forEach(function (e, i) {
+//        textAlign(LEFT);
+//        fill(e.color);
+//        noStroke();
+//        ellipse(20, 50 + i * 20, 10, 10);
+//        text(e.text, 40, 55 + i * 20);
+//    });
+//}
+
+var TimelineDot = function (decade) {
+    this.decade = decade;
+    this.count = 0;
+    this.wars = [];
+    this.index = 0;
+    this.position = {};
+    this.active = false;
+    this.selected = false;
+}
+TimelineDot.prototype = {
+    addWar: function (war) {
+        this.wars.push(war);
+        this.count++;
+    }
+}
+
+function TimeLine(ctx) {
+    this.ctx = ctx;
+    this.width = this.ctx.width;
+    this.height = this.ctx.height;
+    this.thickness = 4;
+    this.gap = 40;
+    this.bottom = 50;
+    this.x = 0;
+    this.y = this.height - this.bottom;
+    this.lineY = this.y + this.thickness / 2;
+    this.slideDot = {};
+    this.timelineDots = timelineDots;
+    this.interval = (this.width - this.gap * 2) / (this.timelineDots.length);
+    this.initSlideDot();
+}
+TimeLine.prototype = {
+    initSlideDot: function () {
+        this.slideDot = {
+            paddingX: 0,
+            width: 30,
+            height: 8,
+            x: 10,
+            y: this.lineY - 4,
+            radius: 6,
+            year: 0,
+            decade: 0,
+            startDecade: 182
+        }
+    },
+    draw: function () {
+        var that = this;
+        this.ctx.fill('#F11B82');
+        this.ctx.noStroke();
+        this.ctx.rect(this.x, this.y, this.width, this.thickness);
+        this.timelineDots.forEach(function (e, i) {
+            var decade = e.decade * 10;
+            that.ctx.ellipse(that.gap + that.interval * i, that.lineY, 15, 15);
+            that.ctx.textAlign(CENTER);
+            if (decade == that.slideDot.decade * 10) {
+                that.ctx.fill('rgba(209,177,32,1)');
+            } else {
+                that.ctx.fill('#F11B82');
+            }
+            that.ctx.text(decade, that.gap + that.interval * i, that.lineY - 20);
+            that.ctx.fill('#F11B82');
+        })
+    },
+    updateSlideDot: function (x) {
+        if (slide) {
+            this.slideDot.x = x - this.slideDot.paddingX;
+            this.ctx.fill('rgba(209,177,32,1)');
+
+        }
+        if (!slide) {
+            this.ctx.fill('rgba(209,177,32,0.8)');
+            this.slideDot.paddingX = 0;
+        }
+        this.ctx.noStroke();
+        this.ctx.rect(this.slideDot.x, this.slideDot.y, this.slideDot.width, this.slideDot.height, 6);
+        if (this.slideDot.x - this.interval / 2 > 0) {
+            this.slideDot.decade = this.slideDot.startDecade + ~~((this.slideDot.x + this.slideDot.width / 2 - this.gap + this.interval / 2) / this.interval);
+        }
+    },
+    mousePosDetect: function () {
+        var x = this.ctx.mouseX;
+        var y = this.ctx.mouseY;
+        if (this.slideDot.x > x) {
+            return false
+        }
+        if (this.slideDot.x + this.slideDot.width < x) {
+            return false
+        }
+        if (this.slideDot.y > y) {
+            return false
+        }
+        if (this.slideDot.y + this.slideDot.height < y) {
+            return false
+        }
+        this.slideDot.paddingX = x - this.slideDot.x;
+        return true;
+    }
+}
+
+
+function Bubble(option) {
+    this.center = option.center;
+    this.radius = option.radius;
+    this.color = option.color;
+    this.info = option.info;
+    this.position = option.position;
+    this.active = false;
+    this.selected = false;
+}
+Bubble.prototype = {
+    draw: function (ctx, countryCenter) {
+
+        this.pixelPos = geo2pixel(ctx, countryCenter[0],countryCenter[1]);
+        this.radius = 10 + sqrt(this.info.batDeath / 1000);
+        this.radius > 50 ? 51 : this.radius;
+
+        fill('#F11B82');
+        noStroke();
+        var l = 30 + this.radius / 2 + '%';
+        var fillColor = 'hsl(60, 80%,' + l + ')';
+        fill(color(fillColor));
+        ellipse(this.pixelPos[0], this.pixelPos[1], this.radius * 2, this.radius * 2);
+    },
+    update: function(country) {
+        if(this.selected) {
+            textSize(13);
+            var infotext = 'War Name:' + this.info.WarName;
+            var cwidth = textWidth(infotext) > textWidth('Start Date:' + parseDate(this.info.startDate)) ? textWidth(infotext) : textWidth('Start Date:' + parseDate(this.info.startDate));
+            infotext +='\n' + 'Casualties:' + this.info.batDeath + '\n' + 'Start Date:' + parseDate(this.info.startDate) + '\n' + 'End Date:' + parseDate(this.info.endDate);
+            cwidth += 20;
+            textAlign(LEFT);
+            fill('rgba(250,50,120,0.4)');
+            rect(this.pixelPos[0] - cwidth / 2 , this.pixelPos[1] - 120, cwidth, 80);
+            fill('black');
+            text(infotext, this.pixelPos[0] - cwidth / 2 + 10, this.pixelPos[1] - 100)
+            //rect(this.x, this.y, this.width, this.thickness);
+        }
+        var mouse = createVector(mouseX, mouseY);
+        if(mouse.dist(createVector(this.pixelPos[0], this.pixelPos[1]))< this.radius && !hovered){
+            hovered = true;
+            selected = this;
+            fill(0);
+            textSize(13);
+            text(country.name, mouseX, mouseY);
+        }
+    }
+}
+
+
+function drawBubbles(decade, ctx) {
+    if (decade && bubbles[decade]) {
+        bubbles[decade].forEach(function (group) {
+            group.draw(ctx);
+        })
+    }
+}
+
+var BubbleGroup = function (option) {
+    this.name = option.name;
+    this.center = option.center;
+    this.participants = option.participants;
+    this.bubbles = [];
+    this.radius = 30;
+}
+BubbleGroup.prototype = {
+    init: function () {
+        var that = this;
+        this.center.x += (Math.random() - 0.5) * 50;
+        this.center.y += (Math.random() - 0.5) * 50;
+        var count = this.participants.length;
+        if (count > 4) {
+            this.radius = 70;
+        }
+        this.participants.forEach(function (e, i) {
+            function getBubblePos(i) {
+                var delta = Math.PI * 2 / count;
+                var pos = {
+                    x: that.center.x + Math.cos(delta * i) * that.radius * (1 + Math.random() / 2),
+                    y: that.center.y + Math.sin(delta * i) * that.radius * (1 + Math.random() / 2)
+                }
+                return pos;
+            }
+
+            function getBubbleRadius(num) {
+                var baseRadius = 0;
+                if (num <= 1000) {
+                    baseRadius = 30;
+                }
+                if (num > 1000 && num <= 10000) {
+                    baseRadius = 40;
+                }
+                if (num > 10000 && num <= 40000) {
+                    baseRadius = 50;
+                }
+                if (num > 40000) {
+                    baseRadius = 60;
+                }
+                return baseRadius + ~~(baseRadius * Math.random() / 2)
+            }
+            var pos = getBubblePos(i);
+            var info = {
+                WarName: that.name,
+                countryName: e.country,
+                startDate: e.startDate,
+                endDate: e.endDate,
+                batDeath: e.batDeath,
+                side: e.side,
+                outcome: e.outcome
+            }
+            var randColor = getRandomColor();
+            var radius = getBubbleRadius(e.batDeath);
+            var option = {
+                center: that.center,
+                position: pos,
+                info: info,
+                color: randColor,
+                radius: radius
+            }
+
+            var bubble = new Bubble(option);
+            that.bubbles.push(bubble);
+        })
+    },
+
+    draw: function (ctx) {
+        this.bubbles.forEach(function (e) {
+            myCountries.forEach(function(country) {
+                if(country.name && country.name == e.info.countryName) {
+                    var countryCenter = getPolygenCenter(country.coordinates[0]);
+                    e.draw(ctx,countryCenter);
+                }
+
+            })
+        })
+        this.bubbles.forEach(function (e) {
+            myCountries.forEach(function(country) {
+                if(country.name && country.name == e.info.countryName) {
+                    e.update(country);
+                }
+
+            })
+        })
+    }
+}
+
+function createBubbles() {
+    timelineDots.forEach(function (e) {
+        var decade = e.decade;
+        bubbles[decade] = [];
+        if (e.wars) {
+            e.wars.forEach(function (war) {
+                var position = {
+                    x: 500,
+                    y: 250
+                };
+                var bubbleGroup = new BubbleGroup({
+                    name: war.name,
+                    center: position,
+                    participants: war.participants
+                });
+                bubbleGroup.init();
+                bubbles[decade].push(bubbleGroup);
+            })
+        }
+    })
+}
+
+var War = function (obj) {
+    this.id = obj.WarNum;
+    this.name = obj.WarName;
+    this.year = obj.StartYear1;
+    this.location = obj.WhereFought;
+    this.decade = ~~(obj.StartYear1 / 10);
+    this.participants = [];
+
+}
+War.prototype = {
+    addParticipant: function (obj) {
+        var participant = {
+            country: obj.StateName,
+            startDate: new Date(obj.StartYear1, obj.StartMonth1, obj.StartDay1),
+            endDate: new Date(obj.EndYear1, obj.EndMonth1, obj.EndDay1),
+            batDeath: obj.BatDeath,
+            side: obj.Side,
+            outcome: obj.Outcome
+        }
+        this.participants.push(participant);
+    }
+}
+
+
+var getWars = function (row) {
+    var obj = row.obj;
+    var isWarExists = wars.some(function (e) {
+        if (e.id == obj.WarNum) {
+            e.addParticipant(obj);
+        };
+        return e.id == obj.WarNum;
+    });
+    if (!isWarExists) {
+        var war = new War(obj);
+        war.addParticipant(obj);
+        wars.push(war);
+    }
+}
+
+var getDots = function () {
+    wars.forEach(function (war) {
+        var isDecadeExists = timelineDots.some(function (dot) {
+            if (dot.decade == war.decade) {
+                dot.addWar(war);
+            }
+            return dot.decade == war.decade;
+        });
+        if (!isDecadeExists) {
+            var dot = new TimelineDot(war.decade);
+            dot.addWar(war);
+            timelineDots.push(dot);
+        }
+    })
+    var index = 0;
+    while (index < timelineDots.length - 1) {
+        if (timelineDots[index].decade + 1 != timelineDots[index + 1].decade) {
+            timelineDots.splice(index + 1, 0, {
+                decade: timelineDots[index].decade + 1
+            });
+        }
+        index += 1;
+    }
+}
+function mouseClicked() {
+    if(lastSelected) {
+        lastSelected.selected = false;
+    }
+    if(selected) {
+        if(lastSelected) {
+            lastSelected.selected = false;
+        }
+        lastSelected = selected;
+        selected.selected = true;
+    }
+}
+
+function mercator(lat, lon) {
+    var scale = 150;
+    var x = radians(lon);
+    var y = log(tan(PI / 4 + radians(lat) / 2));
+    x *= scale;
+    y *= scale;
+    x += width / 2;
+    y = height / 2 - y + 50;
+    vertex(x, y);
+}
+function geo2pixel(ctx, lon, lat) {
+    var scale = 150;
+    var x = ctx.radians(lon);
+    var y = log(ctx.tan(PI / 4 + ctx.radians(lat) / 2));
+    x *= scale;
+    y *= scale;
+    x += width / 2;
+    y = height / 2 - y + 50;
+    return ([x,y]);
+}
+
+function getRandomColor() {
+    var r = ~~(Math.random() * 255);
+    var g = ~~(Math.random() * 255);
+    var b = ~~(Math.random() * 255);
+    return 'rgba(' + r + ',' + g + ',' + b + ',0.5)';
+}
+
+function timeLineSlideStart() {
+    if (timeLine.mousePosDetect()) {
+        slide = true;
+    }
+}
+
+function timeLineSlideEnd() {
+    slide = false;
+}
+
+function timeLineSlide() {
+    if (!slide) {
+        return timeLine.slideDot.x;
+    }
+    return mouseX;
+}
+function getPolygenCenter(arr) {
+    var lon = 0;
+    var lat = 0;
+    arr.forEach(function(e) {
+        lon += e[0];
+        lat += e[1];
+    });
+    var l = arr.length;
+    return ([lon / l, lat / l]);
+}
+
+function parseDate(d) {
+    var y = d.getFullYear();
+    var m = d.getMonth() + 1;
+    var d = d.getDate();
+    return y + '-' + m + '-' + d;
+}
